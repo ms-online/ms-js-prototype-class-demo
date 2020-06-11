5996c8e6b611f95e9c5fc8a98ec55e873cc90379
misterwu <27732357@qq.com>
2020-06-11 19:34:11 +0800

    lesson-3

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ function UI() {}
 // 添加课程
 UI.prototype.addCourseToList = function (course) {
   const list = document.getElementById("course-list");
-  // 创建
+  // 创建tr标签
   const row = document.createElement("tr");
-  // 插入
+  // 插入表格
   row.innerHTML = `
       <td>${course.title}</td>
       <td><a href="${course.link}">查看</a></td>
@@ -24,24 +24,61 @@ UI.prototype.addCourseToList = function (course) {
   list.appendChild(row);
 };
 
+// 弹窗提醒
+UI.prototype.showAlert = function (message, className) {
+  // 创建div
+  const div = document.createElement("div");
+  // 添加类
+  div.className = `alert ${className}`;
+  // 添加文本
+  div.appendChild(document.createTextNode(message));
+  // 获取父级元素
+  const container = document.querySelector(".container");
+  // 获取表单
+  const form = document.querySelector("#course-form");
+  // 插入dom
+  container.insertBefore(div, form);
+
+  // 清除
+  setTimeout(function () {
+    document.querySelector(".alert").remove();
+  }, 3000);
+};
+
+// 清除
 UI.prototype.clearFields = function () {
   document.getElementById("title").value = "";
   document.getElementById("link").value = "";
   document.getElementById("isbn").value = "";
 };
 
+// 事件
 document.getElementById("course-form").addEventListener("submit", function (e) {
+  // 获取表单值
   const title = document.getElementById("title").value,
     link = document.getElementById("link").value,
     isbn = document.getElementById("isbn").value;
 
+  // 初始化
   const course = new Course(title, link, isbn);
 
+  // 初始化
   const ui = new UI();
 
-  ui.addCourseToList(course);
+  // 校验
+  if (title == "" || link == "" || isbn == "") {
+    // 弹窗提醒
+    ui.showAlert("请填写必填项", "error");
+  } else {
+    // 添加课程
+    ui.addCourseToList(course);
 
-  ui.clearFields();
+    // 弹窗提醒
+    ui.showAlert("添加成功", "success");
+
+    // 清除
+    ui.clearFields();
+  }
 
   e.preventDefault();
 });
